fbcc330bb8b4745a4388af6f2023ec2bf23af261
KRoushanKumar <roushank559@gmail.com>
2023-07-14 23:22:48 +0530

    Employee can close query

--- src/main/java/com/helpDeskPortal/HDP/controller/EmpQueryController.java
+++ src/main/java/com/helpDeskPortal/HDP/controller/EmpQueryController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -134,6 +135,17 @@ public class EmpQueryController {
 		
 		empQueryService.addEmployeeQuery(tempEmpQuery);
 		
+	}
+	
+	@PutMapping("/closeEmployeeQuery/{empQueryId}")
+	public void closeEmployeeQuery(@PathVariable int empQueryId)
+	{
+		EmployeeQuery tempEmpQuery = new EmployeeQuery();
+		tempEmpQuery = empQueryService.getQueryByID(empQueryId);
+		tempEmpQuery.setPriority(priorityService.findById(2));
+		tempEmpQuery.setProgress(progressService.findById(3));
+		System.out.println(" tempEmpQuery "+tempEmpQuery);
+		empQueryService.addEmployeeQuery(tempEmpQuery);
 	}
 	
 	@GetMapping("/queryById/{id}")
